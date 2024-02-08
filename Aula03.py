9fb3ef1abe98db481ffc290dc9de4de47191cb9d
TheXerife <103859044+TheXerife@users.noreply.github.com>
2024-02-08 08:48:34 -0400

    Update Aula03.py

--- Aula03.py
+++ Aula03.py
@@ -1,4 +1,4 @@
-#1. Faça um programa que imprima o seu nome.
+# 1. Faça um programa que imprima o seu nome.
 
 # nome = input(f" Digite seu nome:")
 
@@ -7,7 +7,7 @@
 # _____________________________________________________________
 
 
-#2. Faça um programa que imprima o produto dos valores 30 e 27.
+# 2. Faça um programa que imprima o produto dos valores 30 e 27.
 
 # print ("Imprimindo o numero: 30")
 # print ("Imprimindo o numero: 27")
@@ -16,9 +16,9 @@
 # _____________________________________________________________
 
 
-#3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
+# 3. Faça um programa que imprima a média aritmética entre os números 5, 8, 12.
 
-# media = (5 + 8 + 12) / 3 
+# media = (5 + 8 + 12) / 3
 
 # print (f" {media:.2f}")
 
@@ -26,7 +26,7 @@
 # _____________________________________________________________
 
 
-#4. Faça um programa que leia e imprima um número inteiro.
+# 4. Faça um programa que leia e imprima um número inteiro.
 
 # numero = int(input("Digite um numero:"))
 
@@ -36,57 +36,65 @@
 # _____________________________________________________________
 
 
-#5. Faça um programa que leia dois números reais e os imprima.
+# 5. Faça um programa que leia dois números reais e os imprima.
 
-# numero1 = float(input("Digite um numero:"))
-# numero2= float(input("Digite um numero:"))
+def Ex5():
 
-# print(numero1, numero2)
+    numero1 = float(input("Digite um numero:"))
+    numero2= float(input("Digite um numero:"))
 
-# _____________________________________________________________
-
-
-#6. Faça um programa que leia um número inteiro e imprima o seu antecessor e o seu sucessor.
-
-# numero = int(input("Digite um numero:"))
-
-# print(numero-1)
+    print(numero1, numero2)
 
 # _____________________________________________________________
 
 
-#7. Faça um programa que leia o nome o endereço e o telefone de um cliente e ao final, imprima esses dados.
+# 6. Faça um programa que leia um número inteiro e imprima o seu antecessor e o seu sucessor.
 
-# nome = input("Digite seu nome: ")
-# endereco = input("Digite seu endereco: ")
-# telefone = input("Digite seu telefone: ")
+def Ex6():
 
-# print (f" {nome}, {endereco}, {telefone}")
+    numero = int(input("Digite um numero:"))
 
-
-# _____________________________________________________________
-
-
-#8. Faça um programa que leia dois números inteiros e imprima a subtração deles.
-
-# num1 = int(input("Digite um numero:"))
-# num2= int(input("Digite um numero:"))
-
-# sub = int(num1-num2)
-
-# print(sub)
+    print(numero-1)
 
 # _____________________________________________________________
 
 
-#9. Faça um programa que leia um número real e imprima ¼ deste número.
+# 7. Faça um programa que leia o nome o endereço e o telefone de um cliente e ao final, imprima esses dados.
+
+def Ex7():
+
+    nome = input("Digite seu nome: ")
+    endereco = input("Digite seu endereco: ")
+    telefone = input("Digite seu telefone: ")
+
+    print (f" {nome}, {endereco}, {telefone}")
+
+# _____________________________________________________________
 
 
-# num = float(input("Digite um numero:"))
+# 8. Faça um programa que leia dois números inteiros e imprima a subtração deles.
 
-# div = float(num/4)
+def Ex8():
 
-# print (div)
+    num1 = int(input("Digite um numero:"))
+    num2= int(input("Digite um numero:"))
+
+    sub = int(num1-num2)
+
+    print(sub)
+
+# _____________________________________________________________
+
+
+# 9. Faça um programa que leia um número real e imprima ¼ deste número.
+
+def Ex9():
+
+    num = float(input("Digite um numero:"))
+
+    div = float(num/4)
+
+    print (div)
 
 # _____________________________________________________________
 
@@ -95,128 +103,136 @@
 #     média aritmética destes números. Ao final, o programa deve
 #     imprimir o resultado do cálculo.
 
+def Ex10():
 
-# num1 = float(input("Digite a primeira nota: "))
-# num2= float(input("Digite  a segunda nota: "))
-# num3= float(input("Digite  a terceira nota: "))
+    num1 = float(input("Digite a primeira nota: "))
+    num2= float(input("Digite  a segunda nota: "))
+    num3= float(input("Digite  a terceira nota: "))
 
-# media = (num1 + num2 + num3)/3
+    media = (num1 + num2 + num3)/3
 
-# print(f"A média do aluno(a) é {media:.2f}")
+    print(f"A média do aluno(a) é {media:.2f}")
 
 # _____________________________________________________________
 
 
-#11. Faça um programa que leia dois números reais e calcule as
+# 11. Faça um programa que leia dois números reais e calcule as
 #    quatro operações básicas entre estes dois números, adição,
 #    subtração,multiplicação e divisão. Ao final, o programa
 #    deve imprimir os resultados dos cálculos.
 
-# num1 = float(input("Digite um numero: "))
-# num2= float(input("Digite um numero: "))
+def Ex11():
 
-# soma = (num1 + num2)
-# sub = (num1 - num2)
-# div = (num1 / num2)
-# mult = (num1 * num2)
+    num1 = float(input("Digite um numero: "))
+    num2= float(input("Digite um numero: "))
 
-# print(f" Soma = {soma:.2f} \n Subtração = {sub:.2f} \n Divisão = {div:.2f} \n Multiplicação = {mult:.2f}")
+    soma = (num1 + num2)
+    sub = (num1 - num2)
+    div = (num1 / num2)
+    mult = (num1 * num2)
+
+    print(f" Soma = {soma:.2f} \n Subtração = {sub:.2f} \n Divisão = {div:.2f} \n Multiplicação = {mult:.2f}")
 
 # _____________________________________________________________
 
-#12. Faça um programa que leia um número real e calcule o
+# 12. Faça um programa que leia um número real e calcule o
 #    quadrado deste número. Ao final, o programa deve
 #    imprimir o resultado do cálculo.
 
+def Ex12():
 
-# num = float(input("Digite um numero:"))
+    num = float(input("Digite um numero:"))
 
-# quadrado = (num**2)
+    quadrado = (num**2)
 
-# print(f" {num}² é {quadrado}")
+    print(f" {num}² é {quadrado}")
 
 # _____________________________________________________________
 
 
-#13. Faça um programa que leia o saldo de uma conta poupança e
+# 13. Faça um programa que leia o saldo de uma conta poupança e
 #    imprima o novo saldo, considerando um reajuste de 2%.
 
-# saldo = float(input("Digite seu saldo da conta:"))
-# ajuste = (saldo * 0.02) + saldo
+def Ex13():
 
-# print(f"Sua conta rendeu 2% esse mes seu novo saldo é {ajuste} ")
+    saldo = float(input("Digite seu saldo da conta:"))
+    ajuste = (saldo * 0.02) + saldo
 
+    print(f"Sua conta rendeu 2% esse mes seu novo saldo é {ajuste} ")
 
 # _____________________________________________________________
 
 
-#14. Faça um programa que leia a base e a altura de um retângulo
+# 14. Faça um programa que leia a base e a altura de um retângulo
 #    e imprima o perímetro (base + altura) e a área (base * altura).
 
-# base = float(input("Digite o tamanho da base: "))
-# altura = float(input("Digite a altura: "))
+def Ex14():
 
-# peririmetro = float(base + altura)
-# area = float(base * altura)
+    base = float(input("Digite o tamanho da base: "))
+    altura = float(input("Digite a altura: "))
 
-# print(f" \n O tamanho do peririmetro desse retângulo é {peririmetro} m² \n O tamanho da area desse retângulo é {area} m² \n \n ")
+    peririmetro = float(base + altura)
+    area = float(base * altura)
 
+    print(f" \n O tamanho do peririmetro desse retângulo é {peririmetro} m² \n O tamanho da area desse retângulo é {area} m² \n \n ")
 
 # _____________________________________________________________
 
 
-#15. Faça um programa que leia o valor de um produto, o percentual
+# 15. Faça um programa que leia o valor de um produto, o percentual
 #    do desconto desejado e imprima o valor do desconto e o valor
 #    do produto subtraindo o desconto.
 
-# valor = float(input("Digite o valor do produto: "))
-# porc = float(input("Digite a porcentagem de desconto (%): "))                                 (Revisar)
+def Ex15():
 
-# desconto = (valor * porc)
-# total = (valor - desconto)
+    valor = float(input("Digite o valor do produto: "))
+    porc = float(input("Digite a porcentagem de desconto (%): "))                                 (Revisar)
 
-# print(f"O valor de desconto é R${porc}")
+    desconto = (valor * porc)
+    total = (valor - desconto)
 
-# print(f"O sub total com o desconto é aplicado é R${total}")
+    print(f"O valor de desconto é R${porc}")
 
+    print(f"O sub total com o desconto é aplicado é R${total}")
 
 # _____________________________________________________________
 
 
-#16. Faça um programa que calcule o reajuste do salário de um
+# 16. Faça um programa que calcule o reajuste do salário de um
 #    funcionário. Para isso, o programa deverá ler o salário atual
 #    do funcionário e ler o percentual de reajuste. Ao final imprimir
 #    o valor do novo salário.
 
+def Ex16():
 
-# salario = float(input("Digite seu salário: "))
-# ajuste = float(input("Digite a porcentagem de ajuste salárial (%): "))
+    salario = float(input("Digite seu salário: "))
+    ajuste = float(input("Digite a porcentagem de ajuste salárial (%): "))
 
 
-# salarioF = (salario * ajuste) + salario
+    salarioF = (salario * ajuste) + salario
 
-# print(f" \n Seu salário com o ajuste aplicado é: R${salarioF:.2f} \n \n ")
+    print(f" \n Seu salário com o ajuste aplicado é: R${salarioF:.2f} \n \n ")
 
 # _____________________________________________________________
 
 
-#17. Faça um programa que calcule a conversão entre graus centígrados
+# 17. Faça um programa que calcule a conversão entre graus centígrados
 #    e Fahrenheit. Para isso, leia o valor em centígrados e calcule
 #    com base na fórmula a seguir. Após calcular o programa deve
 #    imprimir o resultado da conversão.
 #    F = (9 x C + 160) / 5
 
-# centigrados = float(input("Digite a temperatura em °C: "))
+def Ex17():
 
-# Fahrenheit = (9 * centigrados + 160) / 5
+    centigrados = float(input("Digite a temperatura em °C: "))
 
+    Fahrenheit = (9 * centigrados + 160) / 5
 
-# print(f"\n A conversão de {centigrados}°C para Fahrenheit é {Fahrenheit}°F \n \n ")
-
+    print(f" A conversão de {centigrados}°C para Fahrenheit é {Fahrenheit}°F \n \n ")
 
 # _____________________________________________________________
 
-#18. Faça um programa que calcule a quantidade de litros de combustível
+# 18. Faça um programa que calcule a quantidade de litros de combustível
 #    consumidos em uma viagem, sabendo-se que o carro tem autonomia de
 #    12 km por litro de combustível. O programa deverá ler o tempo
 #    decorrido na viagem e a velocidade média e aplicar as fórmulas:
@@ -229,57 +245,60 @@
 #    Ao final, o programa deverá imprimir a distância percorrida e a
 #    quantidade de litros consumidos na viagem.
 
+def Ex18():
 
-# T = float(input("Digite o tempo de viagem em horas: "))
-# V = float(input("Digite a velocidade média (Km/h): "))
+    T = float(input("Digite o tempo de viagem em horas: "))
+    V = float(input("Digite a velocidade média (Km/h): "))
 
-# D = (T * V)
-# L = (D / 12)
+    D = (T * V)
+    L = (D / 12)
 
-# texto = f'''
+    texto = f'''
 
-# {D} km = Distância percorrida em horas 
-# {T} h = Tempo 
-# {V} km/h = Velocidade média 
-# {L:.2f} l =  Litros de combustível consumidos  
+    {D} km = Distância percorrida em horas
+    {T} h = Tempo
+    {V} km/h = Velocidade média
+    {L:.2f} l =  Litros de combustível consumidos
 
-# '''
+    '''
 
-# print(texto)
+    print(texto)
 
 
 # _____________________________________________________________
 
-#19. Faça um programa que calcule o valor de uma prestação em atraso.
+# 19. Faça um programa que calcule o valor de uma prestação em atraso.
 #    Para isso, o programa deve ler o valor da prestação vencida, a
 #    taxa periódica de juros e o período de atraso. Ao final, o
 #    programa deve imprimir o valor da prestação atrasada, o período
 #    de atraso, os juros que serão cobrados pelo período de atraso, o
 #    valor da prestação acrescido dos juros. Considere juros simples.
 
+def Ex19():
+
+    c = float(input("Digite o valor do boleto: "))
+    i = float(input("Digite a taxa de juros (%): "))
+    t = float(input("Digite o tempo de atraso (mês): "))
+
+    # juros = C * i * t
+
+    juros = (c*(i/100)*t)
+
+    print(juros+c)
 
 
-# c = float(input("Digite o valor do boleto: "))
-# i = float(input("Digite a taxa de juros (%): "))
-# t = float(input("Digite o tempo de atraso (mês): "))
-
-# # J = C ∙ i ∙ t
-
-
-
-# juros = (c*(i/100)*t)
-
-# print(juros+c)
-
-
-#20. Faça um programa que efetue a apresentação do valor da conversão
+# 20. Faça um programa que efetue a apresentação do valor da conversão
 #    em real (R$) de um valor lido em dólar (US$). Para isso, será
 #    necessário também ler o valor da cotação do dólar.
 
-# dolar = float(input("Digite um valor em dolar para conversão ($): "))
+def Ex20():
 
-# conversao = dolar*4.98 
+    dolar = float(input("Digite um valor em dolar para conversão ($): "))
 
-# print(f"A conversão de ${dolar} para reais é R${conversao:.2f}")
+    conversao = dolar*4.98
+
+    print(f"A conversão de ${dolar} para reais é R${conversao:.2f}")
 
 # _____________________________________________________________
+
+Ex20()
